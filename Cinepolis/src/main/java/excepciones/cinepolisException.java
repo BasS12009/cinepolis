7a18a83b3c566414015de8ec250a28c4e3392070
stae01 <166432863+stae01@users.noreply.github.com>
2024-06-04 22:07:10 -0700

    Clase Exception

---
+++ Cinepolis/src/main/java/excepciones/cinepolisException.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author 
+ */
+public class cinepolisException extends Exception {
+
+    public cinepolisException() {
+    }
+
+    public cinepolisException(String message) {
+        super(message);
+    }
+
+    public cinepolisException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+    
+}
