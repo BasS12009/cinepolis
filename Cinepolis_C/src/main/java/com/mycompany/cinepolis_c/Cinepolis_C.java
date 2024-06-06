7a8292163320d59f818764d62d016459fa247809
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-06-06 11:11:41 -0700

    Proyectos
    De nuevo todo

---
+++ Cinepolis_C/src/main/java/com/mycompany/cinepolis_c/Cinepolis_C.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cinepolis_c;
+
+/**
+ *
+ * @author diana
+ */
+public class Cinepolis_C {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
